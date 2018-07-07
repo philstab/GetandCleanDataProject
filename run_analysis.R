67774833f28df74ca47cd80d3fbef0504aa5addb
Phil Stablein <pstable1@jhmi.edu>
2018-07-06 23:35:06 -0400

    step 1 complete

--- run_analysis.R
+++ run_analysis.R
@@ -15,6 +15,42 @@ str(featurenames)
 
 #read fwf for X_train and use features as col names
 x_train <- "./GetandCleanData/project/UCI HAR Dataset/train/X_train.txt"
-x_t <- read_fwf(file=x_train,
+x_trn <- read_fwf(file=x_train,
                fwf_empty(x_train, col_names = featurenames)
                )
+#repeat for X_test
+x_test <- "./GetandCleanData/project/UCI HAR Dataset/test/X_test.txt"
+x_tst <- read_fwf(file=x_test,
+                  fwf_empty(x_test, col_names = featurenames)
+)
+
+dim(x_trn);dim(x_tst)
+
+#read in subject and activity
+train_subject <- scan("./GetandCleanData/project/UCI HAR Dataset/train/subject_train.txt", what=integer())
+train_activity <- scan("./GetandCleanData/project/UCI HAR Dataset/train/y_train.txt", what=integer())
+
+test_subject <- scan("./GetandCleanData/project/UCI HAR Dataset/test/subject_test.txt", what=integer())
+test_activity <- scan("./GetandCleanData/project/UCI HAR Dataset/test/y_test.txt", what=integer())
+
+#column bind the subject and activity to respective data frames, rename
+testdf <- as_tibble(cbind(test_subject, test_activity, x_tst))
+traindf <- as_tibble(cbind(train_subject, train_activity, x_trn))
+
+#clean up global env
+#rm(list=c("x_trn", "x_tst"))
+
+#check subject selection/randomization
+unique(test_subject)
+unique(train_subject)
+
+#add selection variable and re-order
+testdf <- testdf %>% mutate(selection = "test") %>% select (test_subject, selection, test_activity, everything())
+traindf <- traindf %>% mutate(selection = "train") %>% select (train_subject, selection, train_activity, everything())
+#rename in-common vars
+testdf <- testdf %>% rename(subject = test_subject, activity = test_activity)
+traindf <- traindf %>% rename(subject = train_subject, activity = train_activity)
+
+#stackem
+all <- as_tibble(rbind(testdf, traindf))
+
